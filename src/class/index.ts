ef4edbe81c9f759dd11a4bc26a1b5415e86d26f3
Luiz Henrique <luizhjramos@outlook.com>
2022-12-02 14:31:05 -0300

    feat: add collaborator public method

--- src/class/index.ts
+++ src/class/index.ts
@@ -8,7 +8,9 @@ export class Enterprise {
     this.cnpj = cnpj;
   }
 
-  addCollaborator() {}
+  addCollaborator(collaborator: Collaborator): void {
+    this.collaborators.push(collaborator);
+  }
 }
 
 export class Collaborator {
@@ -16,8 +18,13 @@ export class Collaborator {
 }
 
 const enterprise1 = new Enterprise('Wiser', '11.111.111/0001-11');
+
 const collaborator1 = new Collaborator('Luiz', 'Ramos');
 const collaborator2 = new Collaborator('Lucas', 'Souza');
 const collaborator3 = new Collaborator('Gislene', 'Souza');
+
+enterprise1.addCollaborator(collaborator1);
+enterprise1.addCollaborator(collaborator2);
+enterprise1.addCollaborator(collaborator3);
 
 console.log(enterprise1);
